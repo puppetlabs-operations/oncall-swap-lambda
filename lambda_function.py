2915d2391f29aaaafa08869d2b5b4701b3094eb0
Eric Zounes <eric.zounes@puppetlabs.com>
2016-01-05 15:32:12 -0800

    Prepare initial lambda skeleton.

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 
 import json
+import requests
 
 print('Loading function')
 
@@ -9,3 +10,5 @@ def lambda_handler(event, context):
     with open('./config.json') as f:
         config = json.loads(f.read())
     print("Received event: " + json.dumps(event, indent=2))
+    s = requests.Session()
+    s.headers.update({'Authorization': 'Token token={key}'.format(key=config['pagerduty_api_key'])})
